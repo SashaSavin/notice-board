e19d0a24058342bf64f960fa1780eefd71ebdc8b
AlexSavin <praise1234@mail.ru>
2019-09-02 14:56:01 +0300

    New updates for Heroku deployment

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def homePageView(request):
-    return HttpResponse('Hello, World!')
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+
 
--- test_project/settings.py
+++ test_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x=y!u-^s((6*bm6m_y(#5kpq&t(5pctwmqhxmonvjxbi+rvf@m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'test_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
